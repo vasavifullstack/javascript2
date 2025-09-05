9ebcf4b335b200b839f43edd017021b02de6c551
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-05 21:24:29 +0530

    add

---
+++ function.js
@@ -0,0 +1,28 @@
+//function is a set of statements .It performs specific task
+ //function creation , function declaration and function calling
+
+ function add()
+ {
+    console.log("10");
+    console.log("20");
+    console.log(10+20);
+ }
+add();
+
+function firstfun()
+{
+    let studentlist=[{
+                       "studentname": "vasavi",
+                       "location ": "kogatam",
+                       "age": "22"
+    },
+    {
+        "studentname": "sreenu",
+    "  location ": "kogatam",
+    "age": "21"
+
+    }];
+    console.log(studentlist);
+}
+firstfun();
+
