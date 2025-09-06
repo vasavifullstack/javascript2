42a8bcef4c7ec37109f74850a113947d9f6a3ae1
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-06 15:50:13 +0530

    life

---
+++ 9.life.js
@@ -0,0 +1,11 @@
+//life
+(
+    function(param1 ,age){
+        console.log("hello world");
+        console.log("hii");
+        console.log("vasavi");
+        console.log(param1);
+        console.log(age);
+
+    }
+)("Tech Training",5);
