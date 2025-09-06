ce55afe2b145bf38016c41ebc6c7daf211460e9b
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-06 14:59:08 +0530

    anonmous

---
+++ 8.anonmous.js
@@ -0,0 +1,15 @@
+//anonmous function is does not have a specific name.
+var test=function(){
+    let name1="vasavi"
+    console.log(name1)
+}
+test();
+
+var test1=function(param1,param2){
+    let a=param1;
+    let b=param2;
+    console.log(param1);
+    console.log(param2);
+}
+    
+test1("hii" ,"how are you?");
