22459ac61a2139a016b2cfee22012749c23ba8ae
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-06 14:20:16 +0530

    funcparam

---
+++ functionparameter.js
@@ -0,0 +1,35 @@
+//function with parameters and arguments.
+
+//addition with parameters.
+function add(param1 , param2){
+    let a=param1;
+    let b=param2;
+    let result=a+b;
+    return result;
+}
+add();
+
+//addition with arguments.
+let a=add(50,30);
+console.log(a);
+
+let b=add(2000,5000);
+console.log(b);
+
+function sub(param1 , param2){
+    let a1=param1;
+    let b1=param2;
+    let result1=a1-b1;
+    return result1;
+}
+
+
+
+
+let a1=sub(9,4);
+console.log(a1);
+
+let b1=sub(3000,1000);
+console.log(b1)
+
+sub();
