e2e55e40e59ee7b91d58a87098dd0a790477c776
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-06 19:20:37 +0530

    debugging

---
+++ 15.debugging-tech.js
@@ -0,0 +1,30 @@
+let foodItem="Pizza";
+console.log(foodItem);
+foodApp("vasavi");
+OrdersApp("TCS-OrderCount is 10");
+//debugger
+
+let deliveryLoc="Hyderabad";
+console.log(deliveryLoc);
+//debugger
+
+let estTime="10 mins";
+console.log(estTime);
+
+//debugger
+
+let isDelAvailable =false;
+console.log(isDelAvailable);
+
+function foodApp(customerName){
+    let item ="CB";
+    console.log(item);
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+function OrdersApp(status){
+    let itemCount =10;
+    console.log(itemCount);
+}
