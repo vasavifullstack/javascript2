4364192e73d4e069049e5197111090cdc75c4df9
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-07 17:33:43 +0530

    primitivedatatype

---
+++ 2.primitive-datatype.js
@@ -0,0 +1,26 @@
+let age=20;
+let price=100;
+
+console.log(age);
+console.log(price);
+
+//string
+let Myname="vasavireddy";
+let surname="yarramachu";
+
+console.log(Myname);
+console.log(surname);
+
+let details=`Age is :${age} my name is ${Myname}`;
+console.log(details);
+
+//boolean true or false
+let married=false;
+let isworking=true;
+
+console.log(married);
+console.log(isworking);
+
+//undefined
+let whatsYourMind;
+console.log(whatsYourMind);
