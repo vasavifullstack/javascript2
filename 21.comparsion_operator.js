992a4351ad37549ae9db466d3aa9d7809cfdb228
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-11 20:31:17 +0530

    comparsion

---
+++ 21.comparsion_operator.js
@@ -0,0 +1,23 @@
+//comparsion operators.
+//==,===,!=,<,>,<=,>=
+
+console.log(1==1);
+
+if(1==100){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+let age=100;
+console.log(age==200);
+console.log(age!=200);
+console.log(age<=100);
+console.log(age>=100);
+console.log(age>100);
+console.log(age<100);
+console.log(1==1);
+console.log(1=="1");
+console.log(1==="1");
+console.log(typeof("1"));
+console.log(typeof(1));
