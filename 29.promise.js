c11172c92fa5f600a7a46efb10e35293379ecc94
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-13 20:25:14 +0530

    add

--- 29.promise.js
+++ 29.promise.js
@@ -1,1 +1,47 @@
-//promise 
+//promise  is an object.by use new keyword promise  .
+//New Promise((resolve,reject)=>{ //executor function is resolve,reject.
+//})argument as executor fun 
+//.then allow chaining.like accept resolve parameter.
+//.catch allow reject parameter.
+
+
+function register() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("i am register for test");
+            resolve("success")
+        }, 5000)
+    })
+}
+
+function login() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("login with usermailID");
+            reject("reject")
+        }, 3000)
+    })
+}
+login().then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
+register().then((data) => { console.log(data) }).catch((err) => console.log(err))
+login().then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
+function login() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("login with usermailID");
+            resolve("success")                                       //reject("reject")
+        }, 3000)
+    })
+}
+login().then((data) => { console.log(data) }).catch((err) => { console.log(err) })
+
+function relogin(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("relogin successfully");
+            resolve("success")
+        },6000)
+    })
+}
+relogin().then((data)=>{console.log(data)})
+//register().then(login()).then(relogin())
