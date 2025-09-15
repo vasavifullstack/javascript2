f5bfab62e5618028cc3f8aab59a3e5cede238480
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-15 21:56:15 +0530

    changes

--- 31.async_await.js
+++ 31.async_await.js
@@ -5,17 +5,61 @@
 // function until the promise is resolved.it makes use to read and write.
 //error handling by usind try and catch block
 
-async function myname(){
+async function myname() {
     return "vasavireddy";
     resolve("success")
 }
 console.log(myname());
 console.log(myname);
-myname().then((x)=>{console.log(x)});
-myname().then((response)=>{console.log(response);});
+myname().then((x) => { console.log(x) });
+myname().then((response) => { console.log(response); });
+function job(){
+    return new Promise((resolve)=>{
+        console.log("Software engineering");
+        setTimeout(()=>{
+            console.log("skills");
+            resolve();
+        },3000)
+
+    })
+}
+async function Engineering(){
+    console.log("technical skills");
+    console.log("HTML,Javascript,CSS");
+}
+job().then((response)=>{
+    //console.log(response);
+});
+Engineering().then(()=>{
+
+}); 
+
+function sum(a,b){
+    return new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            resolve(a+b);
+        },4000)
+    })
+}
+console.log(sum());
+sum(10,20).then(resolve=>{console.log(resolve);})
 
 
 
+function studentDetails(){
+    return new Promise((resolve,reject)=>{
+        console.log("vasavi details")
+        setTimeout(function(){
+            resolve("B.TECH");
+            console.log("education background");
+        },5000)
+    })
+}
+async function placements(){
+    return await studentDetails();
+    //resolve1("course");
 
-
-
+}
+console.log(placements());
+console.log(studentDetails());
+studentDetails().then(resolve=>{console.log(resolve);})
